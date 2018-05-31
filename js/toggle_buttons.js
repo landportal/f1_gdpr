19ddb80b65d3fa43253ab245401eb3a235f637f6
Andrew Bielefeldt <abielefeldt@forumone.com>
2018-05-31 16:10:22 -0400

    Only attempt to hide buttons that exist on page

--- js/toggle_buttons.js
+++ js/toggle_buttons.js
@@ -1,10 +1,14 @@
 var gdpr_accept_button = document.getElementById('gdpr-accept-cookies');
 var gdpr_decline_button = document.getElementById('gdpr-decline-cookies');
 
-if(has_consent) {
-  gdpr_accept_button.style.display = "none";
+if(has_consent > 0) {
+  if(gdpr_accept_button) {
+    gdpr_accept_button.style.display = "none";
+  }
 } else {
-  gdpr_decline_button.style.display = "none";
+  if(gdpr_decline_button) {
+    gdpr_decline_button.style.display = "none";
+  }
 }
 
 function gdpr_accept_cookies() {
